1a44162acaf75e2dfccaca926cc723896063dbf9
koryuoh <526248620@qq.com>
2020-08-13 14:54:43 +0800

    deploy

---
+++ precache-manifest.b7a455e024f9b692dcaa21f1fe700036.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9578b7847b7a41cefeee",
+    "url": "/Morney-website/css/app.a5074939.css"
+  },
+  {
+    "revision": "ec5b7475d43459cf8781",
+    "url": "/Morney-website/css/chunk-vendors.f2e5169d.css"
+  },
+  {
+    "revision": "861eea84fe6c66216f46549e746ec79a",
+    "url": "/Morney-website/index.html"
+  },
+  {
+    "revision": "9578b7847b7a41cefeee",
+    "url": "/Morney-website/js/app.b9497bfa.js"
+  },
+  {
+    "revision": "ec5b7475d43459cf8781",
+    "url": "/Morney-website/js/chunk-vendors.4f1ede8c.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/Morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Morney-website/robots.txt"
+  }
+]);
